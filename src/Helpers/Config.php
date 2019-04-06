cbcc16ad4741e719bf6cf125e5e27218a87e7efd
Devin Lewis <devinlewis@gmail.com>
2019-04-05 18:38:18 -0600

    work

--- src/Helpers/Config.php
+++ src/Helpers/Config.php
@@ -16,13 +16,15 @@ class Config
             require_once($dir . '/../../config_map.php');
             $host_parts = explode('.', $_SERVER['HTTP_HOST']);
             $site = isset($host_parts[count($host_parts) - 3]) ? $host_parts[count($host_parts) - 3] : null;
-            if (isset($config_map[$site])) {
-                $config = $config_map[$site];
-                foreach ($config as $key => $value) {
-                    putenv("{$key}={$value}");
-                }
+            $service = isset($host_parts[count($host_parts) - 4]) ? $host_parts[count($host_parts) - 4] : 'web';
+            if (isset($config_map['sites'][$site])) {
+                $config = $config_map['sites'][$site];
+                $db_name = $config_map['db_names'][$service];
+                putenv('DB_DATABASE=' . $config['DB_USERNAME'] . '_' . $db_name);
+                putenv('DB_USERNAME=' . $config['DB_USERNAME']);
+                putenv('DB_PASSWORD=' . $config['DB_PASSWORD']);
             } else {
-                die('Config map key not found');
+                die('Config map key (' . $site . ') not found');
             }
         } else {
             die('No config map file found');
