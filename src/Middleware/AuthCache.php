15d08b1b999aa0af300213eedad65c270b6da250
Devin Lewis <devinlewis@gmail.com>
2019-04-23 17:49:39 -0600

    work

--- src/Middleware/AuthCache.php
+++ src/Middleware/AuthCache.php
@@ -22,7 +22,7 @@ class AuthCache
         if (!empty($request->header('x-sh-token'))) {
             $crypt = new Encrypter(env('APP_KEY'), 'AES-256-CBC');
             $timestamp = $crypt->decrypt($request->header('x-sh-token'));
-            if ( is_int($timestamp) && time() <= $timestamp + 300 ) {
+            if ( is_int($timestamp) && time() <= $timestamp + 600 ) {
                 $is_authorized = true;
             }
         }
