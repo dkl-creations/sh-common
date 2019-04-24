3142b784a441c288f886e14332cd1abc78c27592
Devin Lewis <devinlewis@gmail.com>
2019-04-24 14:52:13 -0600

    work

--- src/Middleware/AuthToken.php
+++ src/Middleware/AuthToken.php
@@ -3,10 +3,30 @@
 namespace Lewisqic\SHCommon\Middleware;
 
 use Closure;
+use Illuminate\Contracts\Auth\Factory as Auth;
+use Illuminate\Auth\GenericUser;
 use Illuminate\Encryption\Encrypter;
 
 class AuthToken
 {
+
+    /**
+     * The authentication guard factory instance.
+     *
+     * @var \Illuminate\Contracts\Auth\Factory
+     */
+    protected $auth;
+
+    /**
+     * Create a new middleware instance.
+     *
+     * @param  \Illuminate\Contracts\Auth\Factory  $auth
+     * @return void
+     */
+    public function __construct(Auth $auth)
+    {
+        $this->auth = $auth;
+    }
 
     /**
      * Handle an incoming request.
@@ -15,11 +35,23 @@ class AuthToken
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next, $guard = null)
     {
         $is_authorized = false;
 
         if (!empty($request->header('x-sh-token'))) {
+
+            $user = null;
+
+            $config_map = include(base_path('../config_map.php'));
+            $crypt = new Encrypter($config_map['master_key'], 'AES-256-CBC');
+            $token = $crypt->decrypt($request->header('x-sh-token'));
+            
+            sd($token);
+
+            $this->auth->viaRequest('api', function ($request) use ($user) {
+                return new GenericUser($user);
+            });
 
         }
 
--- src/Middleware/BeforeCors.php
+++ src/Middleware/BeforeCors.php
@@ -23,7 +23,7 @@ class BeforeCors
         }
         $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'authorization');
+        $response->headers->set('Access-Control-Allow-Headers', 'x-sh-token');
         return $response;
     }
 }
