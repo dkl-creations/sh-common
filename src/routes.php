60758fd7c07d9f88350b2d4c4d6dde41628be65e
Devin Lewis <devinlewis@gmail.com>
2019-04-24 14:42:12 -0600

    work

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,7 @@
 <?php
 
 // enter custom routes here
-$this->app->router->group(['prefix' => 'v1', 'middleware' => 'auth-cache', 'namespace' => 'Lewisqic\SHCommon\Controllers'], function () {
+$this->app->router->group(['prefix' => 'v1', 'middleware' => 'auth-identity', 'namespace' => 'Lewisqic\SHCommon\Controllers'], function () {
 
     $this->app->router->post('identity/cache/{id}', ['uses' => 'IdentityController@createCache']);
     $this->app->router->delete('identity/cache/{id}', ['uses' => 'IdentityController@deleteCache']);
