616ad42e9d8a84b2f386501d53383ed69c81e32a
Devin Lewis <devinlewis@gmail.com>
2019-04-23 20:48:59 -0600

    work

---
+++ src/Helpers/Identity.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Lewisqic\SHCommon\Helpers;
+
+use Illuminate\Encryption\Encrypter;
+
+class Identity
+{
+
+    /**
+     * Create the token cache on each MS for a given user
+     *
+     * @param $user
+     */
+    public static function createTokenCache($user)
+    {
+        $config_map = include(base_path() . '/../config_map.php');
+
+        foreach ($config_map['services'] as $service => $data) {
+
+            $crypt = new Encrypter($data['key'], 'AES-256-CBC');
+            $token = $crypt->encrypt(time());
+
+            //s($service);
+            //s($token);
+
+            $response = Api::post($service, 'v1/identity/cache/create', $user, [
+                'headers' => [
+                    'X-SH-Token' => $token
+                ]
+            ]);
+
+        }
+
+        die();
+
+    }
+
+}
--- src/routes.php
+++ src/routes.php
@@ -6,6 +6,9 @@ $this->app->router->group(['prefix' => 'v1', 'middleware' => 'auth-cache'], func
     $this->app->router->post('identity/cache/create', function() {
         \Log::debug($_POST);
 
+        // delete old cached files
+        // create new cache file for the given user
+        // file username is an md5 hash or user ID
 
     });
 
