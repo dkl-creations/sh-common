dbb0645eccece4e91be8a56416d5f52783ee3cb9
Devin Lewis <devinlewis@gmail.com>
2019-04-30 16:38:53 -0600

    work

--- src/Helpers/Config.php
+++ src/Helpers/Config.php
@@ -16,27 +16,31 @@ class Config
                 $config_map = include(base_path('../config_map.php'));
 
                 $host_parts = explode('.', $_SERVER['HTTP_HOST']);
-                $service = $host_parts[count($host_parts) - 4];
-                $org = $org_data != null ? $org_data['domain'] : '';
+                if (isset($host_parts[count($host_parts) - 4])) {
+                    $service = $host_parts[count($host_parts) - 4];
+                    $org = $org_data != null ? $org_data['domain'] : '';
 
-                $db_database = $config_map['services'][$service]['db_table'];
-                if (isset($config_map['db_credentials']['services'][$service])) {
-                    $config = $config_map['db_credentials']['services'][$service];
-                    $db_username = $config['DB_USERNAME'];
-                    $db_password = $config['DB_PASSWORD'];
-                } elseif (isset($config_map['db_credentials']['organizations'][$org])) {
-                    $config = $config_map['db_credentials']['organizations'][$org];
-                    $db_username = $config['DB_USERNAME'];
-                    $db_password = $config['DB_PASSWORD'];
-                    $db_database = preg_replace('/\{username\}/', $db_username, $db_database);
-                }
+                    if (isset($config_map['services'][$service]['db_table'])) {
+                        $db_database = $config_map['services'][$service]['db_table'];
+                        if ( isset($config_map['db_credentials']['services'][$service]) ) {
+                            $config = $config_map['db_credentials']['services'][$service];
+                            $db_username = $config['DB_USERNAME'];
+                            $db_password = $config['DB_PASSWORD'];
+                        } else if ( isset($config_map['db_credentials']['organizations'][$org]) ) {
+                            $config = $config_map['db_credentials']['organizations'][$org];
+                            $db_username = $config['DB_USERNAME'];
+                            $db_password = $config['DB_PASSWORD'];
+                            $db_database = preg_replace('/\{username\}/', $db_username, $db_database);
+                        }
 
-                if (isset($db_username)) {
-                    config(['database.connections.mysql.database' => $db_database]);
-                    config(['database.connections.mysql.username' => $db_username]);
-                    config(['database.connections.mysql.password' => $db_password]);
-                } else {
-                    die('Unable to locate database credentials');
+                        if ( isset($db_username) ) {
+                            config(['database.connections.mysql.database' => $db_database]);
+                            config(['database.connections.mysql.username' => $db_username]);
+                            config(['database.connections.mysql.password' => $db_password]);
+                        } else {
+                            die('Unable to locate database credentials');
+                        }
+                    }
                 }
 
             } else {
