05a3a556b4b3943cf3ff5b0917f17f2710dee643
Devin Lewis <devinlewis@gmail.com>
2019-05-02 12:04:38 -0600

    work

--- src/Commands/DbSetCommand.php
+++ src/Commands/DbSetCommand.php
@@ -21,6 +21,20 @@ class DbSetCommand extends Command
     protected $description = 'Set the organization before executing an artisan command';
 
     /**
+     * Set the db username to be used
+     *
+     * @var string
+     */
+    protected $db_username = '';
+
+    /**
+     * Set the db name to be used
+     *
+     * @var string
+     */
+    protected $db_database = '';
+
+    /**
      * Create a new command instance.
      *
      * @return void
@@ -45,15 +59,28 @@ class DbSetCommand extends Command
         $cmd = $this->option('cmd');
         if (empty($cmd)) {
             $cmd = $this->choice('Which command to run?', [
-                'migrate', 'migrate:rollback', 'db:seed'
+                'migrate',
+                'migrate:fresh',
+                'migrate:install',
+                'migrate:refresh',
+                'migrate:reset',
+                'migrate:rollback',
+                'migrate:status',
+                'db:seed',
+                'passport:install',
+                'passport:keys',
+                'passport:client',
+                'passport:purge',
             ]);
         }
+
+        $options = $this->ask('Do you want to pass in any options?');
 
         if (isset($config_map['db_credentials']['services'][$this_service])) {
             $creds = $config_map['db_credentials']['services'][$this_service];
 
             $this->loadOrgCredentials($db_name, $creds);
-            $this->runCommand($cmd);
+            $this->runCommand($cmd, $options);
 
         } else {
             $orgs = [
@@ -66,26 +93,34 @@ class DbSetCommand extends Command
             if ($org == 'all') {
                 foreach ($all_orgs as $org_name => $creds) {
                     $this->loadOrgCredentials($db_name, $creds);
-                    $this->runCommand($cmd);
+                    $this->runCommand($cmd, $options);
                 }
             } else {
                 $creds = $config_map['db_credentials']['organizations'][$org];
                 $this->loadOrgCredentials($db_name, $creds);
-                $this->runCommand($cmd);
+                $this->runCommand($cmd, $options);
             }
         }
-
 
     }
 
     /**
      * Run the specified command now
      */
-    protected function runCommand($cmd, $options = [])
+    protected function runCommand($cmd, $options_string = null)
     {
-
+        $options = [];
+        if ($options_string != null) {
+            $option_parts = explode(' ', $options_string);
+            if (is_array($option_parts)) {
+                foreach ($option_parts as $option) {
+                    $parts = explode('=', $option);
+                    $options[$parts[0]] = isset($parts[1]) ? $parts[1] : true;
+                }
+            }
+        }
+        $this->question('Running command: "' . strtoupper($cmd . ' ' . $options_string) . '" for user: "' . strtoupper($this->db_username) . '" on the database: "' . strtoupper($this->db_database) . '"');
         $this->call($cmd, $options);
-
     }
 
     /**
@@ -96,6 +131,8 @@ class DbSetCommand extends Command
     protected function loadOrgCredentials($db_name, $creds)
     {
         $database = preg_replace('/\{username\}/', $creds['DB_USERNAME'], $db_name);
+        $this->db_username = $creds['DB_USERNAME'];
+        $this->db_database = $database;
         config(['database.connections.mysql.database' => $database]);
         config(['database.connections.mysql.username' => $creds['DB_USERNAME']]);
         config(['database.connections.mysql.password' => $creds['DB_PASSWORD']]);
