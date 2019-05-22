a935ec7c1c895fe97a74ba90691c50b193d904b3
Devin Lewis <devinlewis@gmail.com>
2019-05-22 17:25:20 -0600

    work

--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -3,9 +3,28 @@
 namespace Lewisqic\SHCommon\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Watson\Validating\ValidatingTrait;
 
 abstract class BaseModel extends Model
 {
+    use ValidatingTrait;
+
+    /**
+     * Whether the model should throw a ValidationException if it
+     * fails validation. If not set, it will default to false.
+     *
+     * @var bool
+     */
+    protected $throwValidationExceptions = true;
+
+    /**
+     * Default validation rules
+     *
+     * @var array
+     */
+    protected $rules = [
+        //
+    ];
 
     /**
      * The attributes that are not mass assignable.
