7ef97f791aca6962f56563b52980357b58e78ebf
Devin Lewis <devinlewis@gmail.com>
2019-05-23 16:24:33 -0600

    work

--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -3,19 +3,23 @@
 namespace Lewisqic\SHCommon\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\DB;
 use Watson\Validating\ValidatingTrait;
+use Lewisqic\SHCommon\Scopes\ContentObjectPermissionsScope;
 
 abstract class BaseModel extends Model
 {
     use ValidatingTrait;
 
     /**
-     * Whether the model should throw a ValidationException if it
-     * fails validation. If not set, it will default to false.
+     * The attributes that should be mutated to dates.
      *
-     * @var bool
+     * @var array
      */
-    protected $throwValidationExceptions = true;
+    protected $dates = [
+        'created_at', 'updated_at', 'deleted_at',
+    ];
 
     /**
      * Default validation rules
@@ -36,12 +40,65 @@ abstract class BaseModel extends Model
     ];
 
     /**
-     * The attributes that should be mutated to dates.
+     * Whether the model should throw a ValidationException if it
+     * fails validation. If not set, it will default to false.
      *
-     * @var array
+     * @var bool
      */
-    protected $dates = [
-        'created_at', 'updated_at', 'deleted_at',
-    ];
+    protected $throwValidationExceptions = true;
+
+    /**
+     * The default order by clause for a model
+     *
+     * @var null
+     */
+    protected static $defaultOrder = null;
+
+    /**
+     * Does this model use content object permissions
+     *
+     * @var bool
+     */
+    protected static $usesContentObjectPermissions = null;
+
+
+    /******************************************************************
+     * MODEL BOOT METHOD
+     ******************************************************************/
+
+    /**
+     * The "booting" method of the model.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        // check for content object permissions
+        if (static::$usesContentObjectPermissions) {
+            static::addGlobalScope(new ContentObjectPermissionsScope);
+        }
+
+        // apply our default order by clause
+        if (!empty(static::$defaultOrder) && is_array(static::$defaultOrder)) {
+            static::addGlobalScope('order_by', function (Builder $builder) {
+                if ( empty($builder->getQuery()->orders) ) {
+                    $builder->orderBy(static::$defaultOrder[0], static::$defaultOrder[1]);
+                }
+            });
+        }
+
+        // listen for model events
+        static::created(function($model) {
+            if (static::$usesContentObjectPermissions && !empty(app('role'))) {
+                // create content object permission record
+                DB::table('content_object_permissions')->insert([
+                    'role_id' => app('role')['id'],
+                    'model_id' => $model->id,
+                    'model_type' => get_class($model),
+                ]);
+            }
+        });
+
+    }
 
 }
