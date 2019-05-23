924426a00c3c153d3a40b82d041b919f0a129b26
Devin Lewis <devinlewis@gmail.com>
2019-05-23 14:44:34 -0600

    work

---
+++ src/Scopes/ContentObjectPermissionsScope.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Lewisqic\SHCommon\Scopes;
+
+use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+
+class ContentObjectPermissionsScope implements Scope
+{
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $builder
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        $table = $model->getTable();
+        $type = get_class($model);
+        if (!empty(app('role'))) {
+            $builder->whereExists(function($query) use($table, $type) {
+                $query->select('*')
+                    ->from('content_object_permissions')
+                    ->where('content_object_permissions.role_id', app('role')['id'])
+                    ->where('content_object_permissions.model_type', $type)
+                    ->whereRaw("content_object_permissions.model_id = {$table}.id");
+            });
+        }
+    }
+}
