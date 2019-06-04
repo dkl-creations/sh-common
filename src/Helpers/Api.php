a38cb0e80906d6425dc99ceb345c9091fd998b63
Devin Lewis <devinlewis@gmail.com>
2019-06-03 20:00:24 -0600

    update api class

--- src/Helpers/Api.php
+++ src/Helpers/Api.php
@@ -16,7 +16,7 @@ class Api
      * @param $args
      * @return mixed
      */
-    public static function __callStatic($method, $args) {
+    /*public static function __callStatic($method, $args) {
 
         $request = app('request');
         $method = strtoupper($method);
@@ -70,6 +70,84 @@ class Api
 
         return $data;
 
+    }*/
+
+    protected static function makeCall($method, $service, $route, $params = [], $options = [])
+    {
+
+        $request = app('request');
+        $url = api_url($service, $route);
+        $data = null;
+
+        try {
+            $http = new Client;
+
+            $request_data = ['http_errors' => false];
+            if (isset($params) && !empty($params)) {
+                if ( $method == 'GET' ) {
+                    $request_data['query'] = $params;
+                } else {
+                    $request_data['form_params'] = $params;
+                }
+            }
+            if (isset($options)) {
+                $request_data = array_merge($request_data, $options);
+            }
+
+            if (!isset($request_data['headers']['Authorization'])) {
+
+                if (!empty($request->header('authorization'))) {
+                    $request_data['headers']['Authorization'] = $request->header('authorization');
+                    $request_data['headers']['Accept'] = $request->header('accept');
+                } else {
+                    $config_map = get_config_map();
+                    $crypt = new Encrypter($config_map['master_key'], 'AES-256-CBC');
+                    $token = $crypt->encrypt([
+                        'host'    => $_SERVER['HTTP_HOST'] ?? '',
+                        'expires_at' => date('Y-m-d H:i:s', strtotime('+5 minutes')),
+                    ]);
+                    $request_data['headers']['Referer'] = $_SERVER['HTTP_HOST'] ?? '';
+                    $request_data['headers']['X-SH-Token'] = $token;
+                }
+
+            }
+
+            $response = $http->request($method, $url, $request_data);
+            $data = json_decode((string)$response->getBody(), true);
+            if (!empty($data['code']) && preg_match('/^4/', $data['code'])) {
+                fail($data['message'], $data['code']);
+            }
+        } catch (BadResponseException $e) {
+            fail($e->getMessage(), $e->getStatusCode());
+        }
+
+        return $data;
+
+    }
+
+    public static function get($service, $route, $params = [], $options = [])
+    {
+        return self::makeCall('GET', $service, $route, $params, $options);
+    }
+
+    public static function post($service, $route, $params = [], $options = [])
+    {
+        return self::makeCall('POST', $service, $route, $params, $options);
+    }
+
+    public static function put($service, $route, $params = [], $options = [])
+    {
+        return self::makeCall('PUT', $service, $route, $params, $options);
+    }
+
+    public static function patch($service, $route, $params = [], $options = [])
+    {
+        return self::makeCall('PATCH', $service, $route, $params, $options);
+    }
+
+    public static function delete($service, $route, $params = [], $options = [])
+    {
+        return self::makeCall('DELETE', $service, $route, $params, $options);
     }
 
 }
