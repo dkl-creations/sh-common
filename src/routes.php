7d39f59bb44483f014c917cfd1d7b226d2b2516a
Devin Lewis <devinlewis@gmail.com>
2019-06-17 16:36:42 -0600

    work

--- src/routes.php
+++ src/routes.php
@@ -2,9 +2,6 @@
 
 $this->app->router->group(['prefix' => 'v1', 'middleware' => 'auth-timestamp', 'namespace' => 'Lewisqic\SHCommon\Controllers'], function () {
 
-    // permissions routes
-    $this->app->router->get('permissions/service', ['uses' => 'PermissionController@listServicePermissions']);
-
     // identity routes
     $this->app->router->post('identity/cache/{id}', ['uses' => 'IdentityController@createCache']);
     $this->app->router->put('identity/cache/{id}', ['uses' => 'IdentityController@updateCache']);
@@ -15,6 +12,7 @@ $this->app->router->group(['prefix' => 'v1', 'middleware' => 'auth-timestamp', '
 $this->app->router->group(['prefix' => 'v1', 'middleware' => 'auth-token', 'namespace' => 'Lewisqic\SHCommon\Controllers'], function () {
 
     // permissions routes
+    $this->app->router->get('permissions/service', ['uses' => 'PermissionController@listServicePermissions']);
     $this->app->router->get('permissions/content/assigned', ['uses' => 'PermissionController@assignedContentPermissions']);
     $this->app->router->put('permissions/content/update', ['uses' => 'PermissionController@updateContentPermissions']);
 
